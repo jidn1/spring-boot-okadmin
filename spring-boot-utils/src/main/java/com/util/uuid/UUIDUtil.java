1f79148e86cbfcc0776071ac5726a063272524ca
正经吉 <dening1644@163.com>
2020-04-10 17:45:11 +0800

    OKadmin code submit

--- spring-boot-utils/src/main/java/com/util/uuid/UUIDUtil.java
+++ spring-boot-utils/src/main/java/com/util/uuid/UUIDUtil.java
@@ -20,39 +20,6 @@ public class UUIDUtil {
         return uuid.toString();
     }
 
-    public void test1() {
-
-        String p1 = "";
-        String p2 = "";
-        String p3 = "";
-        String sql = "select * from table as a where 1=1 AND c.dealtime >= trunc (sysdate)";
-        if (p1 != null || p2 != null || p3 != null) {
-            if (p1 != null && p2 != null) {
-                sql += "and ( 1=1 ";
-            }
-            if (p1 != null || p2 != null) {
-                sql += " and (  1=1 ";
-                if (p1 != null) {
-                    sql += " and a.hesiRemainDay >= " + p1;
-                }
-                if (p2 != null) {
-                    sql += " and a.hesiRemainDay >= " + p2;
-                }
-                sql += " ) ";
-            }
-            if (p3 != null) {
-                if (p1 != null && p2 != null) {
-                    sql += "or";
-                } else {
-                    sql += "and";
-                }
-                sql += "  a.riskcode IN ('" + p3 + "') ";
-            }
-            sql += " ) ";
-        }
-
-    }
-
     public static String[] chars = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
     public static String generateShortUuid() {
@@ -65,5 +32,9 @@ public class UUIDUtil {
         }
         return shortBuffer.toString();
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateShortUuid());
     }
 }
