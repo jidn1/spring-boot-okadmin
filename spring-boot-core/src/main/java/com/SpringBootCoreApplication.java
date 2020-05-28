4f9291fa19082668132f1eeaa3a9d758d76d8372
正经吉 <dening1644@163.com>
2020-05-28 09:55:25 +0800

    OKadmin code submit

--- spring-boot-core/src/main/java/com/SpringBootCoreApplication.java
+++ spring-boot-core/src/main/java/com/SpringBootCoreApplication.java
@@ -2,7 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@SpringBootApplication
 public class SpringBootCoreApplication {
 
     public static void main(String[] args) {
